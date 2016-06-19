9888c610053727145d4cb53ee5b61c7c20e8731c
MateuszGalus <mateuszgalus@gmail.com>
2016-06-20 01:34:34 +0200

    Basic styling

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,10 @@
 class Booking < ActiveRecord::Base
 	belongs_to :car
 	validates :customer_name, presence: true
-	validate :uniqueness_of_date_range
+	validate :date_validation
 
 	private
-	def uniqueness_of_date_range
+	def date_validation
 		errors.add(:booked_from, "is not available") unless Booking.where("? >= booked_from AND ? <= booked_to",
 															booked_from, booked_from).count == 0
 		errors.add(:booked_to, "is not available") unless Booking.where("? >= booked_from AND ? <= booked_to",
